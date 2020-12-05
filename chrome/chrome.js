7a1c1d3383e79b22ffc152ac24d7ae8df0250a5c
triplewy <triplewy@gmail.com>
2020-12-05 16:57:50 -0500

    Save images

--- chrome/chrome.js
+++ chrome/chrome.js
@@ -52,18 +52,21 @@ const TMP_DIR = Path.join(DATA_PATH, 'tmp');
 const TIMINGS_DIR = Path.join(DATA_PATH, 'timings');
 const WPROFX_DIR = Path.join(DATA_PATH, 'wprofx');
 const NETLOG_DIR = Path.join(DATA_PATH, 'netlog');
+const IMAGE_DIR = Path.join(DATA_PATH, 'images');
 
 fs.mkdirSync(TMP_DIR, { recursive: true });
 fs.mkdirSync(TIMINGS_DIR, { recursive: true });
 fs.mkdirSync(WPROFX_DIR, { recursive: true });
 fs.mkdirSync(NETLOG_DIR, { recursive: true });
+fs.mkdirSync(IMAGE_DIR, { recursive: true });
 
 const DOMAINS = CONFIG.domains;
 const SINGLE_SIZES = CONFIG.sizes.single;
 const MULTI_SIZES = CONFIG.sizes.multi;
 
 // const DOMAINS = ['cloudflare'];
-// const SIZES = ['medium'];
+// const SINGLE_SIZES = ['100KB'];
+// const MULTI_SIZES = ['small'];
 
 const CHROME_PROFILE = Path.join(TMP_DIR, 'chrome-profile');
 const TMP_NETLOG = Path.join(TMP_DIR, 'chrome.json');
@@ -119,6 +122,7 @@ const chromeArgs = (urls) => {
         '--headless',
         '--disable-gpu',
         '--disable-dev-shm-usage',
+        '--window-size=1920,1080',
         `--user-data-dir=${CHROME_PROFILE}`,
         '--disk-cache-dir=/dev/null',
         '--disk-cache-size=1',
@@ -250,7 +254,6 @@ const runBenchmark = async (urlString, timingsDir, netlogDir, isH3) => {
         fs.mkdirSync(timingsDir, { recursive: true });
     }
     const realNetlogDir = Path.join(netlogDir, `chrome_${isH3 ? 'h3' : 'h2'}_single`);
-
     if (!fs.existsSync(realNetlogDir)) {
         fs.mkdirSync(realNetlogDir, { recursive: true });
     }
@@ -289,7 +292,7 @@ const runBenchmark = async (urlString, timingsDir, netlogDir, isH3) => {
     });
 };
 
-const runChromeWeb = async (urlObj, netlogDir, wprofxDir, isH3, n) => {
+const runChromeWeb = async (urlObj, netlogDir, wprofxDir, imageDir, isH3, n) => {
     const { url: urlString, size } = urlObj;
 
     const domains = [urlString];
@@ -330,7 +333,7 @@ const runChromeWeb = async (urlObj, netlogDir, wprofxDir, isH3, n) => {
                             extends: 'lighthouse:default',
                             settings: {
                                 additionalTraceCategories: TRACE_CATEGORIES.join(','),
-                                onlyAudits: LIGHTHOUSE_CATEGORIES,
+                                onlyAudits: LIGHTHOUSE_CATEGORIES.concat('screenshot-thumbnails'),
                                 throttlingMethod: 'provided',
                                 emulatedFormFactor: 'none',
                             },
@@ -411,6 +414,14 @@ const runChromeWeb = async (urlObj, netlogDir, wprofxDir, isH3, n) => {
                     });
 
                     // fs.writeFileSync(`/tmp/lighthouse/${isH3 ? 'H3' : 'H2'}-report-${i}.html`, report);
+                    const realImageDir = Path.join(imageDir, `request_${i}`);
+                    if (!fs.existsSync(realImageDir)) {
+                        fs.mkdirSync(realImageDir, { recursive: true });
+                    }
+                    audits['screenshot-thumbnails'].details.items.forEach((item, k) => {
+                        const base64Data = item.data.replace(/^data:image\/jpeg;base64,/, '');
+                        fs.writeFileSync(Path.join(realImageDir, `image_${k}.jpeg`), base64Data, 'base64');
+                    });
 
                     break;
                 } catch (error) {
@@ -444,7 +455,7 @@ const runChromeWeb = async (urlObj, netlogDir, wprofxDir, isH3, n) => {
     return timings;
 };
 
-const runBenchmarkWeb = async (urlObj, timingsDir, wprofxDir, netlogDir, isH3) => {
+const runBenchmarkWeb = async (urlObj, timingsDir, wprofxDir, netlogDir, imageDir, isH3) => {
     let timings = {
         plt: [],
     };
@@ -466,7 +477,11 @@ const runBenchmarkWeb = async (urlObj, timingsDir, wprofxDir, netlogDir, isH3) =
     if (!fs.existsSync(realWprofxDir)) {
         fs.mkdirSync(realWprofxDir, { recursive: true });
     }
+    const realImageDir = Path.join(imageDir, `chrome_${isH3 ? 'h3' : 'h2'}`);
 
+    if (!fs.existsSync(realImageDir)) {
+        fs.mkdirSync(realImageDir, { recursive: true });
+    }
 
     // Read from timings file if exists
     const file = Path.join(timingsDir, `chrome_${isH3 ? 'h3' : 'h2'}.json`);
@@ -483,7 +498,7 @@ const runBenchmarkWeb = async (urlObj, timingsDir, wprofxDir, netlogDir, isH3) =
     const prevLength = timings['speed-index'].length;
 
     // Run benchmark
-    const result = await runChromeWeb(urlObj, realNetlogDir, realWprofxDir, isH3, prevLength);
+    const result = await runChromeWeb(urlObj, realNetlogDir, realWprofxDir, realImageDir, isH3, prevLength);
 
     // Concat result times to existing data
     Object.keys(timings).forEach((key) => {
@@ -514,6 +529,14 @@ const runBenchmarkWeb = async (urlObj, timingsDir, wprofxDir, netlogDir, isH3) =
             fs.unlinkSync(Path.join(realWprofxDir, f));
         }
     });
+
+    // Remove image directories that are not median
+    fs.readdirSync(realImageDir).forEach((d) => {
+        const i = parseInt(d.split('_')[1], 10);
+        if (!medianIndexes.has(i)) {
+            deleteFolderRecursive(Path.join(realImageDir, d));
+        }
+    });
 };
 
 (async () => {
@@ -540,6 +563,7 @@ const runBenchmarkWeb = async (urlObj, timingsDir, wprofxDir, netlogDir, isH3) =
             const timingsDir = Path.join(TIMINGS_DIR, dir, domain, size);
             const wprofxDir = Path.join(WPROFX_DIR, dir, domain, size);
             const netlogDir = Path.join(NETLOG_DIR, dir, domain, size);
+            const imageDir = Path.join(IMAGE_DIR, dir, domain, size);
 
             console.log(`${domain}/${size}`);
 
@@ -550,9 +574,9 @@ const runBenchmarkWeb = async (urlObj, timingsDir, wprofxDir, netlogDir, isH3) =
                 await runBenchmark(urlObj, timingsDir, netlogDir, true);
             } else {
                 console.log('Chrome: H3 - multi object');
-                await runBenchmarkWeb(urlObj, timingsDir, wprofxDir, netlogDir, true);
+                await runBenchmarkWeb(urlObj, timingsDir, wprofxDir, netlogDir, imageDir, true);
                 console.log('Chrome: H2 - multi object');
-                await runBenchmarkWeb(urlObj, timingsDir, wprofxDir, netlogDir, false);
+                await runBenchmarkWeb(urlObj, timingsDir, wprofxDir, netlogDir, imageDir, false);
             }
         }
     }
--- run_benchmark.sh
+++ run_benchmark.sh
@@ -8,6 +8,6 @@ BW=$3
 
 DIRNAME="loss-${LOSS}_delay-${DELAY}_bw-${BW}"
 
-node $BASEDIR/chrome/chrome.js --single --dir $DIRNAME
-python3 client.py --dir $DIRNAME
+# node $BASEDIR/chrome/chrome.js --single --dir $DIRNAME
+# python3 client.py --dir $DIRNAME
 node $BASEDIR/chrome/chrome.js --no-single --dir $DIRNAME
