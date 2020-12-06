4409d964e91861b352da4c91c5949129057a0e01
triplewy <triplewy@gmail.com>
2020-12-06 16:47:36 -0500

    Add network scripts

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -2,11 +2,7 @@
 
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-LOSS=$1
-DELAY=$2
-BW=$3
-
-DIRNAME="loss-${LOSS}_delay-${DELAY}_bw-${BW}"
+DIRNAME=$1
 
 python3 client.py --dir $DIRNAME
 HOME=/quic node $BASEDIR/chrome/chrome.js --single --dir $DIRNAME
--- run_benchmarks.sh
+++ run_benchmarks.sh
@@ -2,47 +2,30 @@
 
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-# 0 loss
-sudo tcdel ens192 --all
-sudo tcset ens192 --rate 100mbps --direction outgoing
-sudo tcset ens192 --rate 100mbps --direction incoming --add
-sudo tcshow ens192
-$BASEDIR/run_benchmark.sh 0 0 100
+NETWORK_CONDITIONS=(
+    "loss-0_delay-0_bw-100"
+    "loss-0dot1_delay-0_bw-100"
+    "loss-1_delay-0_bw-100"
+    "loss-1burst_delay-0_bw-100"
+    "loss-1_delay-50_bw-100"
+    "loss-0_delay-50_bw-100"
+    "loss-0_delay-100_bw-100"
+    "loss-0_delay-100jitter_bw-100"
+    "loss-0_delay-0_bw-10"
+    "loss-0dot1_delay-0_bw-10"
+    "loss-1_delay-0_bw-10"
+    "loss-1burst_delay-0_bw-10"
+    "loss-1_delay-50_bw-10"
+    "loss-0_delay-50_bw-10"
+    "loss-0_delay-100_bw-10"
+    "loss-0_delay-100jitter_bw-10"
+)
 
-# 0.1% loss
-sudo tcdel ens192 --all
-sudo tcset ens192 --rate 100mbps --loss 0.1% --direction outgoing
-sudo tcset ens192 --rate 100mbps --loss 0.1% --direction incoming --add
-sudo tcshow ens192
-$BASEDIR/run_benchmark.sh 0dot1 0 100
-
-# 1% loss
-sudo tcdel ens192 --all
-sudo tcset ens192 --rate 100mbps --loss 1% --direction outgoing
-sudo tcset ens192 --rate 100mbps --loss 1% --direction incoming --add
-sudo tcshow ens192
-$BASEDIR/run_benchmark.sh 1 0 100
-
-# 50ms delay
-sudo tcdel ens192 --all
-sudo tcset ens192 --rate 100mbps --delay 25ms --direction outgoing
-sudo tcset ens192 --rate 100mbps --delay 25ms --direction incoming --add
-sudo tcshow ens192
-$BASEDIR/run_benchmark.sh 0 50 100
-
-# 100ms delay
-sudo tcdel ens192 --all
-sudo tcset ens192 --rate 100mbps --delay 50ms --direction outgoing
-sudo tcset ens192 --rate 100mbps --delay 50ms --direction incoming --add
-sudo tcshow ens192
-$BASEDIR/run_benchmark.sh 0 100 100
-
-# 1% loss + 50ms delay
-sudo tcdel ens192 --all
-sudo tcset ens192 --rate 100mbps --delay 25ms --loss 1% --direction outgoing
-sudo tcset ens192 --rate 100mbps --delay 25ms --loss 1% --direction incoming --add
-sudo tcshow ens192
-$BASEDIR/run_benchmark.sh 1 50 100
+for network in "${NETWORK_CONDITIONS[@]}"; do
+    echo "$network"
+    $BASEDIR/network/"$network".sh
+    $BASEDIR/run_benchmark.sh $network
+done
 
 # Analysis
 python3 $BASEDIR/main.py
